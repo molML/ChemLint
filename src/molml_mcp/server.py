a7ba6fc91274c40ca9a428f97d554241d012ab7e
derekvantilborg <dwvantilborg@gmail.com>
2025-11-28 15:43:12 +0100

    no ghetto sys path insertion

--- src/molml_mcp/server.py
+++ src/molml_mcp/server.py
@@ -1,16 +1,8 @@
-# from .tools import get_all_tools
-
-# src/molml_mcp/server.py
-
-
-# server.py
 from mcp.server.fastmcp import FastMCP
-from molml_mcp.tools import canonicalize_smiles
 
 # All tools we want to expose via the MCP server
 from molml_mcp.resources.logistics import get_all_resource_functions
-
-
+from molml_mcp.tools import canonicalize_smiles, canonicalize_smiles_dataset
 
 # create an MCP server 
 mcp = FastMCP("molml-mcp") 
@@ -19,8 +11,7 @@ mcp = FastMCP("molml-mcp")
 for tool_func in get_all_resource_functions():
     mcp.add_tool(tool_func)
 
-
 # Add tools
 mcp.add_tool(canonicalize_smiles)
-
+mcp.add_tool(canonicalize_smiles_dataset)
 

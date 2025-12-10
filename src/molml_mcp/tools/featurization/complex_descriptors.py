d57661ad93cdf6adce7a165c93060aa0f8e2ed71
derekvantilborg <dwvantilborg@gmail.com>
2025-12-10 10:30:59 +0100

    remove smiles_to_ecfp since mcp cannot serialize np arrays. This should be an internal function

--- src/molml_mcp/tools/featurization/complex_descriptors.py
+++ src/molml_mcp/tools/featurization/complex_descriptors.py
@@ -1,11 +1,10 @@
 
-from molml_mcp.tools.featurization.supported.ecfps import smiles_to_ecfp, smiles_to_ecfp_dataset
+from molml_mcp.tools.featurization.supported.ecfps import smiles_to_ecfp_dataset
 
 
 def get_all_complex_descriptor_tools():
-    """Return a list of all molecular cleaning tools."""
+    """Return a list of all complex descriptor tools for MCP registration."""
     return [
-        smiles_to_ecfp,
         smiles_to_ecfp_dataset
     ]
 

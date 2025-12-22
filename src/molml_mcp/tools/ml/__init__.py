499ffff6571d6afc7890b177f8403bcf6f1bbf6f
derekvantilborg <dwvantilborg@gmail.com>
2025-12-22 13:08:05 -0500

    added to server

--- src/molml_mcp/tools/ml/__init__.py
+++ src/molml_mcp/tools/ml/__init__.py
@@ -3,6 +3,8 @@
 from molml_mcp.tools.ml.metrics import calculate_metrics, list_all_supported_metrics
 from molml_mcp.tools.ml.evaluation import predict_ml_model
 from molml_mcp.tools.ml.training import train_ml_model
+from molml_mcp.tools.ml.hyperparam_tuning import tune_hyperparameters
+from molml_mcp.tools.ml.trad_ml_models import get_hyperparameter_space, get_all_hyperparameter_spaces
 
 
 def get_all_ml_tools():
@@ -12,4 +14,7 @@ def get_all_ml_tools():
         list_all_supported_metrics,
         predict_ml_model,
         train_ml_model,
+        tune_hyperparameters,
+        get_hyperparameter_space,
+        get_all_hyperparameter_spaces,
     ]
